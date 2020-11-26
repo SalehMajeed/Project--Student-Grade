9fae4e7c9542aa37e160f47950a142b0a8e50af5
= <saleh.majeed8@gmail.com>
2020-11-26 15:26:07 +0530

    student grades

--- js/script.js
+++ js/script.js
@@ -4,5 +4,29 @@ const calcy = () => {
   let comp = document.getElementById('comp').value;
   let phy = document.getElementById('phy').value;
 
-  let total_grades = wd + maths + comp + phy;
+  let total_grades = Number(wd) + Number(maths) + Number(comp) + Number(phy);
+
+  let percentage = (total_grades / 400) * 100;
+
+  let grades;
+
+  if (percentage <= 100 && percentage >= 80) {
+    grades = 'A';
+  } else if (percentage < 79 && percentage >= 60) {
+    grades = 'B';
+  } else if (percentage < 60 && percentage >= 40) {
+    grades = 'C';
+  } else {
+    grades = 'F';
+  }
+
+  if (percentage >= 39.5) {
+    document.getElementById(
+      'showData'
+    ).innerHTML = `Out of 400 your total is ${total_grades} and percentage is ${percentage}%. <br> Your Grade is ${grades}. Your are pass.`;
+  } else {
+    document.getElementById(
+      'showData'
+    ).innerHTML = `Out of 400 your total is ${total_grades} and percentage is ${percentage}%. <br> Your Grade is ${grades}. Your are fail.`;
+  }
 };
