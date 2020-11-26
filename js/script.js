202fe277ee6f8015f1b7eca97da755985b67ee89
= <saleh.majeed8@gmail.com>
2020-11-26 15:05:17 +0530

    html and css

---
+++ js/script.js
@@ -0,0 +1,8 @@
+const calcy = () => {
+  let wd = document.getElementById('wd').value;
+  let maths = document.getElementById('maths').value;
+  let comp = document.getElementById('comp').value;
+  let phy = document.getElementById('phy').value;
+
+  let total_grades = wd + maths + comp + phy;
+};
